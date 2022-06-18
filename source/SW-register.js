fdcfd7f531300899aa054cda6205192394d3a92c
Lete114 <me@imlete.cn>
2022-06-18 14:11:51 +0800

    更新SW

--- source/SW-register.js
+++ source/SW-register.js
@@ -1,4 +1,22 @@
-(async function () {
-  if (!"serviceWorker" in navigator) return;
-  await navigator.serviceWorker.register("/sw.js");
-})();
+(function () {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((result) => {
+        // 判断是否安装了sw
+        if (!localStorage.getItem('installSW')) {
+          localStorage.setItem('installSW', true)
+          // 这里就不用清理setInterval了，因为页面刷新后就没有了
+          setInterval(() => {
+            // 判断sw安装后，是否处于激活状态，激活后刷新页面
+            if (result && result.active && result.active.state === 'activated') {
+              location.reload()
+            }
+          }, 100)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+})()
