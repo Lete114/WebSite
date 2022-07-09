640a30149a0581a87e45ed5c29e3c5bff035e294
Lete114 <me@imlete.cn>
2022-07-09 20:57:24 +0800

    fix: sW 导致页面无限刷新的 bug

--- source/SW-register.js
+++ source/SW-register.js
@@ -7,10 +7,11 @@
         if (!localStorage.getItem('installSW')) {
           localStorage.setItem('installSW', true)
           // 这里就不用清理setInterval了，因为页面刷新后就没有了
-          setInterval(() => {
+          const timer = setInterval(() => {
             // 判断sw安装后，是否处于激活状态，激活后刷新页面
             if (result && result.active && result.active.state === 'activated') {
-              location.reload()
+              clearInterval(timer)
+              location.reload() // sw注册后，会在下次访问时才工作，所以这里调用reload()刷新一次页面
             }
           }, 100)
         }
